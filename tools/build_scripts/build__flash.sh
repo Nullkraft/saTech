c35a8b641380a8c055d0240417ea8f11646f0998
Mark Stanley <1367957+Nullkraft@users.noreply.github.com>
2026-07-26 14:28:57 -0400

    Create bash script to rm w25n_flash library cache

---
+++ tools/build_scripts/build__flash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <pio run arguments>"
+    echo "Example: $0 -e technician -t upload --upload-port /dev/ttyUSB0"
+    exit 1
+fi
+
+rm -rf .pio/libdeps/technician/W25N_Flash_library/
+pio run -t clean && pio run "$@"
